119a9e352f371bc9e6f124797cf5747529da8ae6
StreetStrider <teamfortresslife@gmail.com>
2016-02-21 19:10:24 +0300

    less.

--- slushfile.js
+++ slushfile.js
@@ -1,13 +1,27 @@
 
+var root   = require('rootpath')(__dirname)
+var target = require('rootpath')(process.cwd())
+var buns   = target.partial('web/buns')
+var build  = target.partial('build')
+
 var gulp = require('gulp')
 var  src = gulp.src
 var  dst = gulp.dest
 
-var root   = require('rootpath')(__dirname)
-var target = require('rootpath')(process.cwd())
+var cat = require('gulp-concat')
+var css = require('./lib/css')
 
 gulp.task('default', () =>
 {
 	return src(root('carcass/**'))
 	.pipe(dst(target()))
+})
+
+gulp.task('less', () =>
+{
+	return src(buns('index/index.less'))
+	.pipe(css.less())
+	.pipe(css.prefix())
+	.pipe(cat('bundle.css'))
+	.pipe(dst(build()))
 })
